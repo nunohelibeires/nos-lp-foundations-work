5e9ba6fa8cb6c1d8212e2487812450fbfe069d60
nunohelibeires <nuno.h.costa@parceiros.nos.pt>
2023-11-04 19:18:28 +0000

    :package: NEW: cleaning module dependent on strategy

--- life_expectancy/__init__.py
+++ life_expectancy/__init__.py
@@ -3,3 +3,5 @@ from pathlib import Path
 
 
 DATA_DIR = Path(__file__).parent / "data"
+TSV_FILE_NAME = "eu_life_expectancy_raw.tsv"
+JSON_FILE_NAME = "eurostat_life_expect.zip"
--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,123 +1,41 @@
-"""Module for cleaning life expectancy data."""
-import argparse
+"""Life Expectancy data cleaning pipeline"""
+
 import pandas as pd
+from life_expectancy import DATA_DIR, JSON_FILE_NAME, TSV_FILE_NAME
+from life_expectancy.data_cleaning.abstract import AbstractCleaner
+from life_expectancy.data_cleaning.clean_json import JSONCleaner
+from life_expectancy.data_cleaning.clean_tsv import TSVCleaner
 
-from life_expectancy.data import load_data, save_data_csv
-from life_expectancy import DATA_DIR
 
+class LifeExpectancyCleaning:
+    def __init__(
+            self,
+            country: str,
+            cleaning_strategy: AbstractCleaner
+    ) -> None:
+        self.country = country
+        self.cleaning_strategy = cleaning_strategy
 
-def clean_data(data: pd.DataFrame, region: str) -> None:
-    """ Clean the data for a given region.
+    def load_clean_export(self) -> None:
+        if self.cleaning_strategy == "tsv":
+            strategy = TSVCleaner(
+                file_name = TSV_FILE_NAME
+            )
+        elif self.cleaning_strategy == "json":
+            strategy = JSONCleaner(
+                file_name = JSON_FILE_NAME
+            )
 
-    Args:
-        region: the name of the region to clean
-    """
-    full_data = (
-        data
-        .pipe(_melt_table)
-        .pipe(_clean_year_value_cols)
-        .pipe(_clean_region_col)
-    )
-    region_data = _select_region(full_data, region).reset_index(drop=True)
+        cleand_data = strategy.clean()
 
-    return region_data
+        self._export_cleand_data_to_csv()
 
-def _melt_table(data: pd.DataFrame) -> pd.DataFrame:
-    """Melt the table to long format.
-    
-    Args:
-        data (pd.DataFrame): The data.
-        
-    Returns:
-        data (pd.DataFrame): The data in long format.
-    """
-
-    # clean column names
-    data[['unit', 'sex', 'age', 'region']] = data['unit,sex,age,geo\\time'].str.split(
-        ',', expand=True
-    )
-    data.drop('unit,sex,age,geo\\time', axis=1, inplace=True)
-
-    # melt table
-    return data.melt(
-        id_vars=['unit', 'sex', 'age', 'region'],
-        var_name='year', value_name='value'
-    )
-
-def _clean_year_value_cols(data: pd.DataFrame) -> pd.DataFrame:
-    """Clean the year and value columns, ensure they are of the correct type and
-        drop rows with missing values.
-
-    Args:
-        data (pd.DataFrame): The data.
-
-    Returns:
-        data (pd.DataFrame): The data with cleaned year and value columns.
-    """
-    # Ensure year is an integer
-    data['year'] = data['year'].str.extract(
-        r'(\d+)', expand=False
-        ).astype(int)
-
-    # Ensure value is a float
-    data['value'] = data['value'].str.extract(
-        r'(\d+.\d+)', expand=False
-        ).astype(float)
-
-    # drop rows with missing values
-    data.dropna(inplace=True)
-
-    return data
-
-def _clean_region_col(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Clean the region column of the data.
-
-    Args:
-        data: The data to clean.
-
-    Returns:
-        The cleaned data.
-    """
-    data['region'] = data['region'].apply(
-        lambda region: region.split('_')[0]
+    def _export_cleand_data_to_csv(self, cleand_data: pd.DataFrame) -> None:
+        cleand_data.to_csv(
+            f'{DATA_DIR}/{self.country.lower()}_life_expectancy.csv',
+            index=False
         )
 
-    return data
-
-def _select_region(data: pd.DataFrame, region: str) -> pd.DataFrame:
-    """Select the region.
-
-    Args:
-        data (pd.DataFrame): The data.
-        region (str): The region to select.
-
-    Returns:
-        data (pd.DataFrame): The data with only the specified region.
-
-    Raises:
-        ValueError: If the region is not in the data.
-    """
-    if region not in data['region'].unique():
-        raise ValueError(f"""
-            The region '{region}' is not found in the data.
-            Please choose from {data['region'].unique().tolist()}""")
-    return data[data['region'] == region]
-
-if __name__ == '__main__': # pragma: no cover
-    parser = argparse.ArgumentParser(description='Clean the data.')
-    parser.add_argument(
-        '-r',
-        '--region',
-        type=str,
-        default='PT',
-        help='The region to select. Defaults to PT.'
-    )
-    args = parser.parse_args()
-
-    cleaned_data = clean_data(
-        data=load_data(DATA_DIR),
-        region=args.region
-    )
-
-    save_data_csv(DATA_DIR, cleaned_data, args.region)
+# TODO: parser
+# TODO: country ENUM
+# TODO: testing
